c28ee6cca951bd701d69da86c039c7ad0e60256a
ユーザー名yuko <ka91001xxx@yahoo.co.jp>
2024-12-13 19:04:40 +0900

    listener

--- src/main/java/servlet/CounterServlet.java
+++ src/main/java/servlet/CounterServlet.java
@@ -3,7 +3,6 @@ package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -13,7 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CounterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	public void init(ServletConfig config) throws ServletException{
+	/*public void init(ServletConfig config) throws ServletException{
 		super.init(config);
 		//訪問回数を表すIntegerインスタンスを新規作成し
 		//アプリケーションスコープに保存
@@ -22,7 +21,7 @@ public class CounterServlet extends HttpServlet {
 		application.setAttribute("count", count);
 		
 		System.out.println("init()が実行されました");
-	}
+	}*/
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
